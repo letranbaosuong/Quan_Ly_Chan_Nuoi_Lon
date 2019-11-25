d3f9714dd7ef84562fa6799daffe383e5b02e1c9
Le Tran Bao Suong <suong.66lab@gmail.com>
2019-11-25 23:58:13 +0700

    update ui quan_ly_lon_nai

--- src/com/gdu/dhpm11/qllon/controller/QuanLyLonController.java
+++ src/com/gdu/dhpm11/qllon/controller/QuanLyLonController.java
@@ -1,4 +1,88 @@
 package com.gdu.dhpm11.qllon.controller;
 
-public class QuanLyLonController {
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
+
+public class QuanLyLonController implements Initializable {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TableColumn<?, ?> tblC_MaTai;
+
+    @FXML
+    private TableColumn<?, ?> tblC_ChuKy_21Ngay;
+
+    @FXML
+    private TableColumn<?, ?> tblC_ChuKy_42Ngay;
+
+    @FXML
+    private TableColumn<?, ?> tblC_ChuKy_84Ngay;
+
+    @FXML
+    private TableColumn<?, ?> tblC_DeDuKien;
+
+    @FXML
+    private DatePicker datePickerNgayNhap;
+
+    @FXML
+    private DatePicker datePickerNgayPhoi;
+
+    @FXML
+    private TextField txtMaTaiLon;
+
+    @FXML
+    private TableColumn<?, ?> tblC_Ta;
+
+    @FXML
+    private TableColumn<?, ?> tblC_ECOLI;
+
+    @FXML
+    private TableColumn<?, ?> tblC_TayGiun;
+
+    @FXML
+    private TableColumn<?, ?> tblC_PARVO;
+
+    @FXML
+    private TableColumn<?, ?> tblC_FE_COC;
+
+    @FXML
+    private TableColumn<?, ?> tblC_Suyen;
+
+    @FXML
+    private TableColumn<?, ?> tblC_PRRS;
+
+    @FXML
+    void initialize() {
+        assert tblC_MaTai != null : "fx:id=\"tblC_MaTai\" was not injected: check your FXML file 'QuanLyLon_AnchorPane.fxml'.";
+        assert tblC_ChuKy_21Ngay != null : "fx:id=\"tblC_ChuKy_21Ngay\" was not injected: check your FXML file 'QuanLyLon_AnchorPane.fxml'.";
+        assert tblC_ChuKy_42Ngay != null : "fx:id=\"tblC_ChuKy_42Ngay\" was not injected: check your FXML file 'QuanLyLon_AnchorPane.fxml'.";
+        assert tblC_ChuKy_84Ngay != null : "fx:id=\"tblC_ChuKy_84Ngay\" was not injected: check your FXML file 'QuanLyLon_AnchorPane.fxml'.";
+        assert tblC_DeDuKien != null : "fx:id=\"tblC_DeDuKien\" was not injected: check your FXML file 'QuanLyLon_AnchorPane.fxml'.";
+        assert datePickerNgayNhap != null : "fx:id=\"datePickerNgayNhap\" was not injected: check your FXML file 'QuanLyLon_AnchorPane.fxml'.";
+        assert datePickerNgayPhoi != null : "fx:id=\"datePickerNgayPhoi\" was not injected: check your FXML file 'QuanLyLon_AnchorPane.fxml'.";
+        assert txtMaTaiLon != null : "fx:id=\"txtMaTaiLon\" was not injected: check your FXML file 'QuanLyLon_AnchorPane.fxml'.";
+        assert tblC_Ta != null : "fx:id=\"tblC_Ta\" was not injected: check your FXML file 'QuanLyLon_AnchorPane.fxml'.";
+        assert tblC_ECOLI != null : "fx:id=\"tblC_ECOLI\" was not injected: check your FXML file 'QuanLyLon_AnchorPane.fxml'.";
+        assert tblC_TayGiun != null : "fx:id=\"tblC_TayGiun\" was not injected: check your FXML file 'QuanLyLon_AnchorPane.fxml'.";
+        assert tblC_PARVO != null : "fx:id=\"tblC_PARVO\" was not injected: check your FXML file 'QuanLyLon_AnchorPane.fxml'.";
+        assert tblC_FE_COC != null : "fx:id=\"tblC_FE_COC\" was not injected: check your FXML file 'QuanLyLon_AnchorPane.fxml'.";
+        assert tblC_Suyen != null : "fx:id=\"tblC_Suyen\" was not injected: check your FXML file 'QuanLyLon_AnchorPane.fxml'.";
+        assert tblC_PRRS != null : "fx:id=\"tblC_PRRS\" was not injected: check your FXML file 'QuanLyLon_AnchorPane.fxml'.";
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
--- src/com/gdu/dhpm11/qllon/model/VacXin.java
+++ src/com/gdu/dhpm11/qllon/model/VacXin.java
@@ -1,4 +1,107 @@
 package com.gdu.dhpm11.qllon.model;
 
+import javafx.beans.property.SimpleStringProperty;
+
 public class VacXin {
+    private SimpleStringProperty Ta;
+    private SimpleStringProperty ECOLI;
+    private SimpleStringProperty Tay_Giun;
+    private SimpleStringProperty PARVO;
+    private SimpleStringProperty FE_COC;
+    private SimpleStringProperty Suyen;
+    private SimpleStringProperty PRRS;
+
+    public VacXin(String ta, String ECOLI, String tay_Giun, String PARVO, String FE_COC, String suyen, String PRRS) {
+        this.Ta = new SimpleStringProperty(ta);
+        this.ECOLI =new SimpleStringProperty(ECOLI) ;
+        this.Tay_Giun = new SimpleStringProperty(tay_Giun);
+        this.PARVO = new SimpleStringProperty(PARVO);
+        this.FE_COC = new SimpleStringProperty(FE_COC);
+        this.Suyen = new SimpleStringProperty(suyen);
+        this.PRRS = new SimpleStringProperty(PRRS);
+    }
+
+    public String getTa() {
+        return Ta.get();
+    }
+
+    public SimpleStringProperty taProperty() {
+        return Ta;
+    }
+
+    public void setTa(String ta) {
+        this.Ta.set(ta);
+    }
+
+    public String getECOLI() {
+        return ECOLI.get();
+    }
+
+    public SimpleStringProperty ECOLIProperty() {
+        return ECOLI;
+    }
+
+    public void setECOLI(String ECOLI) {
+        this.ECOLI.set(ECOLI);
+    }
+
+    public String getTay_Giun() {
+        return Tay_Giun.get();
+    }
+
+    public SimpleStringProperty tay_GiunProperty() {
+        return Tay_Giun;
+    }
+
+    public void setTay_Giun(String tay_Giun) {
+        this.Tay_Giun.set(tay_Giun);
+    }
+
+    public String getPARVO() {
+        return PARVO.get();
+    }
+
+    public SimpleStringProperty PARVOProperty() {
+        return PARVO;
+    }
+
+    public void setPARVO(String PARVO) {
+        this.PARVO.set(PARVO);
+    }
+
+    public String getFE_COC() {
+        return FE_COC.get();
+    }
+
+    public SimpleStringProperty FE_COCProperty() {
+        return FE_COC;
+    }
+
+    public void setFE_COC(String FE_COC) {
+        this.FE_COC.set(FE_COC);
+    }
+
+    public String getSuyen() {
+        return Suyen.get();
+    }
+
+    public SimpleStringProperty suyenProperty() {
+        return Suyen;
+    }
+
+    public void setSuyen(String suyen) {
+        this.Suyen.set(suyen);
+    }
+
+    public String getPRRS() {
+        return PRRS.get();
+    }
+
+    public SimpleStringProperty PRRSProperty() {
+        return PRRS;
+    }
+
+    public void setPRRS(String PRRS) {
+        this.PRRS.set(PRRS);
+    }
 }
